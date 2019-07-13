c007c6287ec99b62e23c2f17aed4d4f64b1fb858
Blake Trapnell <Blake.Trapnell103@gmail.com>
2019-07-13 01:25:41 -0600

    css overhaul

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import "./App.css";
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      stats: {}
+
+    }
+  }
+
+
+render() {
+    return (
+      <div className="page">
+      <body>
+        <div className="leftpage"></div>
+        <main>
+      <header>
+        <nav></nav>
       </header>
-    </div>
+          <div className="charactersheet">
+            <div className="cSHeader">
+              <div className="cSHeader1">
+                <li id= "charName">IonSteel</li>
+              </div>
+              <div className="cSHeader2">
+                  <li className="user">Bard 4</li>
+                  <li className="user">Sailor</li>
+                  <li className="user">Blake T</li>
+                  <li className="userbot">Tiefling</li>
+                  <li className="userbot">Chaotic Neut</li>
+                  <li className="userbot">300</li>
+              </div>
+            </div>
+            <div className="cSMain"></div>
+        </div>
+        </main>
+      <footer></footer>
+        <div className="rightpage"></div>
+
+      </body>
+
+
+
+      </div>
   );
 }
-
-export default App;
+}
